a263097d52bd57372bfa4faf79e8d64d84274e3e
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-03-23 16:16:23 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+
+export default{
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<template>
+  <AppHeader/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {}
+    }
+}
+</script>
+
+<!-- html -->
+<template>
+    <div>
+        Logo
+    </div>
+    <div>
+        <ul>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+            <li>MENU</li>
+        </ul>
+    </div>
+</template>
