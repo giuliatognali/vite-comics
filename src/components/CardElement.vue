0cd0e44f9709c2a5c0e17e0326427b95a9fc2702
giuliatognali <118864811+giuliatognali@users.noreply.github.com>
2023-03-27 17:20:14 +0200

    card props

---
+++ src/components/CardElement.vue
@@ -0,0 +1,24 @@
+<script>
+
+export default {
+    name: 'CardElement',
+    props:{
+        img: String,
+        name: String,
+        link: String
+    }
+
+}
+</script>
+
+<template>
+    <div>
+        <a :href="link">
+            <img :src="img" :alt="name">
+            <p>{{ name }}</p>
+        </a>
+    </div>
+</template>
+ 
+
+<style lang="scss" scoped></style>
--- src/components/ComicList.vue
+++ src/components/ComicList.vue
@@ -1,6 +1,88 @@
 <script>
+import CardElement from "./CardElement.vue";
 export default {
-    name: 'ComicList'
+    name: 'ComicList',
+    components: {
+        CardElement
+    },
+    data() {
+        return {
+            comics: [
+                {
+                    "thumb": "https://static.dc.com/2023-03/LEGIONOB_Cv1_00111_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "DC'S LEGION OF BLOOM",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/FLS_Cv795_79511_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "THE FLASH",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/NTW_Cv102_10211_DIGITAL-1.jpg?w=640",
+                    "link": "#",
+                    "series": "NIGHTWING",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/BMSMWF_Cv13_01311_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "BATMAN/SUPERMAN: WORLD'S FINEST",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/CTW_Cv53_05311_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "CATWOMAN",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/BA_Cv9_00911_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "BLACK ADAM",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/SM_LOST_Cv1_00111_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "SUPERMAN: LOST",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/SUPES_Cv2_00211_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "SUPERMAN",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/ROBIN_v3%20%28Cover%29.jpg?w=640",
+                    "link": "#",
+                    "series": "ROBIN VOL. 3: SECRETS AND SHADOWS ",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/HQv3%20%28%20Cover%29.jpg?w=640",
+                    "link": "#",
+                    "series": "HARLEY QUINN VOL. 3: VERDICT",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/MULTVTJ%20%28COVER%29.jpg?w=640",
+                    "link": "#",
+                    "series": "MULTIVERSITY: TEEN JUSTICE",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.dc.com/2023-03/WW_Cv797_79711_DIGITAL.jpg?w=640",
+                    "link": "#",
+                    "series": "WONDER WOMAN",
+                    "type": "graphic novel"
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -11,30 +93,14 @@ export default {
                 <h2>CURRENT SERIES</h2>
             </div>
             <div class="container-small">
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
-                <div class="card-comic">
-                    CARD
-                </div>
+                <CardElement 
+                class="card-comic" 
+                v-for="comic in comics" 
+                :link="comic.link"
+                :img="comic.thumb"
+                :name="comic.series"
+                />
+                
             </div>
             <div class="container">
                 <button class="btn-load">
@@ -67,38 +133,37 @@ section {
         }
 
         .container-small {
-            background-color: pink;
             display: flex;
-            align-items: center;
+            align-items: flex-start;
             flex-wrap: wrap;
             padding-top: 60px;
 
-
-            .card-comic {
-                background-color: $primary-color;
-                width: calc(100% / 6);
-                height: 220px;
-                margin: 10px 15px;
-
-            }
-
         }
-.container{
-    display: flex;
-    justify-content: center;
-    
-    .btn-load {
+
+        .container {
+            display: flex;
+            justify-content: center;
+
+            .btn-load {
                 background-color: $primary-color;
                 height: 40px;
                 width: 185px;
                 border: none;
                 color: white;
                 padding: 15px 30px;
-            
+
                 margin: 20px;
             }
-}
+        }
 
     }
 }
+
+.card-comic {
+    width: calc((100% / 6) - 30px);
+    color: white;
+    margin: 10px 15px;
+}
+
+
 </style>
